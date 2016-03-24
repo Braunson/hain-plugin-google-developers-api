165e754777ea76a6c59e1393a26e2f1a67ff35e4
Braunson Yager <braunson@gmail.com>
2016-03-24 11:52:33 -0400

    Adjusted formatting..

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 'use strict';
 
 module.exports = (pluginContext) = > {
-	const shell = pluginContext.shell;
+    const shell = pluginContext.shell;
 
-	function search(query, res) {
-		const query_trim = query.trim();
-		if (query_trim.length == 0) {
-			return;
-		}
-		res.add({
-			id: query_trim,
-			payload: 'open',
-			title: 'Search ' + query_trim,
-			desc: 'Search in the Google Developers API'
-		});
-	}
+    function search(query, res) {
+        const query_trim = query.trim();
+        if (query_trim.length == 0) {
+            return;
+        }
+        res.add({
+            id: query_trim,
+            payload: 'open',
+            title: 'Search ' + query_trim,
+            desc: 'Search in the Google Developers API'
+        });
+    }
 
-	function execute(id, payload) {
-		if (payload !== 'open') {
-			return;
-		}
-		shell.openExternal(`https: //developers.google.com/s/results/?q=${id}`);
-	}
+    function execute(id, payload) {
+        if (payload !== 'open') {
+            return;
+        }
+        shell.openExternal(`https: //developers.google.com/s/results/?q=${id}}`);
+        }
 
-	return {search, execute};
-};
+        return {
+            search, execute
+        };
+    };
